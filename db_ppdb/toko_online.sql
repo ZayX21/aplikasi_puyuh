3597027747e12d99123924dce6613b164b220e61
ZayX21 <fauzi.fani223@gmail.com>
2023-10-10 11:11:23 +0700

    update db

--- db_ppdb/toko_online.sql
+++ db_ppdb/toko_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2023 at 05:30 AM
+-- Generation Time: Oct 10, 2023 at 06:10 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ppdb`
+-- Database: `toko_online`
 --
 
 -- --------------------------------------------------------
@@ -37,6 +37,14 @@ CREATE TABLE `detail_pesanans` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `detail_pesanans`
+--
+
+INSERT INTO `detail_pesanans` (`id`, `pesanan_id`, `produk_id`, `harga_produk`, `jumlah_dipesanan`, `total_harga`, `created_at`, `updated_at`) VALUES
+(1, 1, 12, 276000.00, 4, 999999.99, '2023-07-25 06:06:02', '2023-07-25 06:06:02'),
+(2, 2, 3, 50000.00, 2, 100000.00, '2023-07-25 19:43:39', '2023-07-25 19:43:39');
 
 -- --------------------------------------------------------
 
@@ -68,6 +76,29 @@ CREATE TABLE `foto_produks` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `foto_produks`
+--
+
+INSERT INTO `foto_produks` (`id`, `produk_id`, `foto`, `created_at`, `updated_at`) VALUES
+(2, 2, '7E0bSlpNZldyUXD5BP9OQQYget5gwDrC5kFB0QH1.jpg', '2023-07-17 07:44:06', '2023-07-17 07:44:06'),
+(3, 3, 'uigQv5VnWPVxRKEvHO6xlRNuSJELlWRJyjB9wPuE.jpg', '2023-07-18 05:24:00', '2023-07-18 05:24:00'),
+(4, 4, 'JSVvn5w9GvAhNny35B3dESa5AdMRmVuvojiRiKtB.jpg', '2023-07-18 05:26:48', '2023-07-18 05:26:48'),
+(5, 5, 'Eeu4UUzCVpROtpMDHvI2jOBS07fb1GcchmCRgqtm.jpg', '2023-07-18 05:29:32', '2023-07-18 05:29:32'),
+(6, 6, 'MWlWYvBL90mqWHNDJXQkP7eAWOxH5YIA0G9AlOKA.jpg', '2023-07-18 05:33:52', '2023-07-18 05:33:52'),
+(7, 7, 'W557DvD2h0ToKIOgyNQpCsfyxtz1VheXbziaeZ0g.jpg', '2023-07-18 05:51:31', '2023-07-18 05:51:31'),
+(8, 8, 'CjE1TWnBKH1CIVop5vGCCRPg9yh1rTPHIRfegURS.jpg', '2023-07-18 05:54:07', '2023-07-18 05:54:07'),
+(9, 10, 'ulBfjwOX5CiOBum4B1XgBXmtxzXpBIBccLyl8mPh.jpg', '2023-07-18 05:59:18', '2023-07-18 05:59:18'),
+(10, 13, '5tDxiiZloSPMI63sNZZ6oHaN1zNhMOKKLCbjsXY0.jpg', '2023-07-18 06:11:18', '2023-07-18 06:11:18'),
+(11, 12, 'ADfCQA0nRiD8srZDlzxs3qfTb5w8ce8DMmgOJmDv.jpg', '2023-07-18 06:11:29', '2023-07-18 06:11:29'),
+(13, 14, 'ntHVAOwrIiJBa46eZlLigPJxBB27QfV1PDjlcZuY.jpg', '2023-07-18 06:16:01', '2023-07-18 06:16:01'),
+(14, 15, 'xWIaMnwZX6dm6ez9i7HJfusCrG2AO8RPzy9qVojb.jpg', '2023-07-18 06:18:52', '2023-07-18 06:18:52'),
+(15, 11, 'TEgNgdYHyMB6T5lnK5FYIyxvC1ilLskicIiVSrk4.jpg', '2023-07-18 06:27:43', '2023-07-18 06:27:43'),
+(16, 16, 'zPjBhqtbtUO35Y5ScNqCkspT3luIO2Fu1GZWeuMt.jpg', '2023-07-18 06:28:05', '2023-07-18 06:28:05'),
+(17, 17, 'ACZr0uSJxJxQl5FFfb31ovPP5tTwQjbjcB6anIYi.jpg', '2023-07-25 05:36:13', '2023-07-25 05:36:13'),
+(18, 19, 'QwcVMe0L6pmmCcgz5G9fuOJxF6l042NZtaWokbB9.jpg', '2023-07-25 05:52:57', '2023-07-25 05:52:57'),
+(19, 18, 'w3YDotYUGWXpePB0UXiyDzrKbv8bjZoiKXk0WAvJ.jpg', '2023-07-25 05:53:11', '2023-07-25 05:53:11');
+
 -- --------------------------------------------------------
 
 --
@@ -80,6 +111,17 @@ CREATE TABLE `kategoris` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kategoris`
+--
+
+INSERT INTO `kategoris` (`id`, `nama`, `created_at`, `updated_at`) VALUES
+(1, 'burung puyuh', '2023-07-17 07:13:13', '2023-07-17 07:13:13'),
+(2, 'obat-obatan', '2023-07-17 07:35:24', '2023-07-17 07:35:24'),
+(3, 'pakan puyuh', '2023-07-17 07:35:30', '2023-07-18 06:49:24'),
+(4, 'telur puyuh', '2023-07-17 07:35:46', '2023-07-17 07:35:46'),
+(5, 'peralatan kandang', '2023-07-17 07:36:00', '2023-07-17 07:36:00');
 
 -- --------------------------------------------------------
 
@@ -94,6 +136,29 @@ CREATE TABLE `kategori_produks` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kategori_produks`
+--
+
+INSERT INTO `kategori_produks` (`id`, `kategori_id`, `produk_id`, `created_at`, `updated_at`) VALUES
+(2, 2, 2, NULL, NULL),
+(3, 2, 3, NULL, NULL),
+(4, 2, 4, NULL, NULL),
+(5, 2, 5, NULL, NULL),
+(6, 3, 6, NULL, NULL),
+(7, 3, 7, NULL, NULL),
+(8, 3, 8, NULL, NULL),
+(11, 4, 11, NULL, NULL),
+(12, 4, 12, NULL, NULL),
+(13, 4, 13, NULL, NULL),
+(14, 5, 14, NULL, NULL),
+(15, 5, 15, NULL, NULL),
+(16, 5, 16, NULL, NULL),
+(18, 3, 10, NULL, NULL),
+(19, 3, 17, NULL, NULL),
+(20, 1, 18, NULL, NULL),
+(21, 1, 19, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -180,6 +245,13 @@ CREATE TABLE `pelanggans` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `pelanggans`
+--
+
+INSERT INTO `pelanggans` (`id`, `user_id`, `no_hp`, `alamat`, `created_at`, `updated_at`) VALUES
+(1, 2, '-', '-', '2023-07-17 07:28:31', '2023-07-17 07:28:31');
+
 -- --------------------------------------------------------
 
 --
@@ -195,6 +267,14 @@ CREATE TABLE `pembayarans` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pembayarans`
+--
+
+INSERT INTO `pembayarans` (`id`, `user_id`, `pesanan_id`, `bukti_pembayaran`, `status`, `created_at`, `updated_at`) VALUES
+(1, 2, 1, 'kLq2tLQkv5IlEIml9EbduOqyBIcepp2wUHzxwtgc.jpg', 'Terkonfirmasi', '2023-07-25 06:06:03', '2023-07-25 06:07:03'),
+(2, 2, 2, 'N1BUeHW5IhQgYwi8iipOUaKFJ18isFdNjp8il5rg.jpg', 'Belum Dikonfirmasi', '2023-07-25 19:43:39', '2023-07-25 19:43:39');
 
 -- --------------------------------------------------------
 
@@ -233,6 +313,14 @@ CREATE TABLE `pesanans` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `pesanans`
+--
+
+INSERT INTO `pesanans` (`id`, `kode_transaksi`, `user_id`, `tanggal`, `alamat_pengiriman`, `total_harga`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'TRX-230725130349-9207', 2, '2023-07-25', 'Grogolan Rt 02/03, Karanggede, Boyolali, Jawa Tengah', 1104000.00, 'Sudah Dikirim', '2023-07-25 06:06:02', '2023-07-25 06:07:13'),
+(2, 'TRX-230726024236-2084', 2, '2023-07-26', 'grogrolan', 100000.00, 'Belum Dikirim', '2023-07-25 19:43:39', '2023-07-25 19:43:39');
+
 -- --------------------------------------------------------
 
 --
@@ -248,6 +336,29 @@ CREATE TABLE `produks` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `produks`
+--
+
+INSERT INTO `produks` (`id`, `nama_produk`, `deskripsi`, `harga`, `stok`, `created_at`, `updated_at`) VALUES
+(2, 'tetra-chlor', 'vita tetra chlor', 4000.00, 10, '2023-07-17 07:41:51', '2023-07-17 07:41:51'),
+(3, 'respirant 100 gr', 'obat CRD komleks untuk snot dan pilek pada ayam dan puyuh', 50000.00, 18, '2023-07-18 05:22:24', '2023-07-25 19:43:39'),
+(4, 'egg max puyuh 10 gr', 'obat untuk meningkatkan jumlah produksi telur pada burung puyuh', 31000.00, 50, '2023-07-18 05:25:22', '2023-07-18 05:25:22'),
+(5, 'coturnix biofit 500 ml', 'obat untuk meningkatkan produksi telur puyuh', 75000.00, 38, '2023-07-18 05:28:43', '2023-07-18 05:28:43'),
+(6, 'NEW HOPE FEED P-100', 'pakan puyuh new hope p-100 untuk puyuh di bawah 7 minggu', 107000.00, 60, '2023-07-18 05:33:01', '2023-07-18 06:30:20'),
+(7, 'NEW HOPE FEED P-500', 'pakan puyuh new hope feed p-500 untuk puyuh dewasa', 377500.00, 100, '2023-07-18 05:51:21', '2023-07-18 06:30:03'),
+(8, 'NEW HOPE FEED P-800', 'pakan puyuh new hope feed p-800', 277500.00, 80, '2023-07-18 05:53:58', '2023-07-18 05:53:58'),
+(10, 'NEW HOPE FEED Py 10', 'pakan anakan puyuh new hope feed py 10 untuk burung puyuh umur 1-6 minggu', 334000.00, 37, '2023-07-18 05:59:10', '2023-07-18 06:49:44'),
+(11, 'Telur puyuh 1 kardus', 'telur puyuh 1 kardus berisi  750 butir telur', 230000.00, 6000, '2023-07-18 06:04:15', '2023-07-18 06:04:15'),
+(12, 'Telur puyuh 1 tray', 'telur puyuh 1 tray berisi 900 butir telur', 276000.00, 7996, '2023-07-18 06:05:08', '2023-07-25 06:06:02'),
+(13, 'Telur puyuh satuan', 'telur puyuh persatuan', 307.00, 8000, '2023-07-18 06:06:26', '2023-07-18 06:06:26'),
+(14, 'nipple', 'nipple untuk alat minum burung puyuh', 7000.00, 300, '2023-07-18 06:15:53', '2023-07-18 06:15:53'),
+(15, 'selang nipple 1 meter', 'selang nipple untuk alat minum burung puyuh', 3250.00, 255, '2023-07-18 06:17:56', '2023-07-18 06:17:56'),
+(16, 'tempat makan puyuh', 'tempat makan puyuh 7 lubang', 17000.00, 299, '2023-07-18 06:20:34', '2023-07-18 06:27:50'),
+(17, 'NEW HOPE FEED Py 100', 'pakan puyuh new hope py 100', 325000.00, 12, '2023-07-25 05:35:43', '2023-07-25 05:35:43'),
+(18, 'puyuh usia 1 minggu', 'burung puyuh muda usia 1 minggu', 5000.00, 1000, '2023-07-25 05:40:11', '2023-07-25 05:40:11'),
+(19, 'burung  puyuh usia 3 minggu', 'burung puyuh muda usia 3 minggu', 7000.00, 2000, '2023-07-25 05:41:36', '2023-07-25 05:41:36');
 
 -- --------------------------------------------------------
 
@@ -304,7 +415,8 @@ CREATE TABLE `role_user` (
 --
 
 INSERT INTO `role_user` (`id`, `role_id`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, '2023-07-17 06:23:41', '2023-07-17 06:23:41');
+(1, 2, 1, '2023-07-17 06:38:29', '2023-07-17 06:38:29'),
+(2, 1, 2, '2023-07-17 07:28:31', '2023-07-17 07:28:31');
 
 -- --------------------------------------------------------
 
@@ -331,7 +443,7 @@ CREATE TABLE `settings` (
 --
 
 INSERT INTO `settings` (`id`, `name`, `favicon`, `logo`, `description`, `email`, `phone`, `address`, `social_media`, `created_at`, `updated_at`) VALUES
-(1, 'Toko Online', NULL, NULL, '-', NULL, NULL, NULL, '\"{\\\"facebook\\\":null,\\\"instagram\\\":null}\"', '2023-07-17 06:23:40', '2023-07-17 06:23:40');
+(1, 'Arifin Puyuh', 'QA6RGrEuuiuVSaCtTTH6iZhQY5GuFDOgw4eHU7yg.png', 'Jq4DY9TIBqFX9nZh0WkMoQhR86ikZjk6cX1nAQwP.jpg', 'Menjual berbagai kebutuhan tentang peternakan Burung puyuh', 'arifinpuyuh@gmail.com', '08213837606', 'simo,boyolali', '\"{\\\"facebook\\\":null,\\\"instagram\\\":null}\"', '2023-07-17 06:38:29', '2023-07-25 06:15:06');
 
 -- --------------------------------------------------------
 
@@ -347,6 +459,13 @@ CREATE TABLE `sliders` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sliders`
+--
+
+INSERT INTO `sliders` (`id`, `judul`, `deskripsi`, `foto`, `created_at`, `updated_at`) VALUES
+(1, 'puyuh usia 1 minggu', 'burung puyuh muda usia 1 minggu', 'KGC0LsQ8EcX0wf4WzxAUfagtrdGSo5BLcqJhu7gB.jpg', '2023-07-25 05:58:11', '2023-07-25 05:58:11');
 
 -- --------------------------------------------------------
 
@@ -370,7 +489,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin@gmail.com', NULL, '$2y$10$jpQ0prZvhZVAsrJkSNUwGOaGnNiwMmOLGp269YUGm3po5jMMPWI96', NULL, '2023-07-17 06:23:41', '2023-07-17 06:23:41');
+(1, 'Admin', 'admin@gmail.com', NULL, '$2y$10$oHYduy9E90MhF1d5r/GNb.vKTff1sMRMJDLpE65r1ZcBMB.TPdlKm', NULL, '2023-07-17 06:38:29', '2023-07-17 06:38:29'),
+(2, 'heru bambang', 'heru123@gmai.com', NULL, '$2y$10$HwOxYhiX2T8DY4G/OYAnWeAg/eGCRLs5/pwBZ.GRtjbF3jSXSnjzC', NULL, '2023-07-17 07:28:31', '2023-07-17 07:28:31');
 
 --
 -- Indexes for dumped tables
@@ -515,7 +635,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `detail_pesanans`
 --
 ALTER TABLE `detail_pesanans`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -527,25 +647,25 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `foto_produks`
 --
 ALTER TABLE `foto_produks`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `kategoris`
 --
 ALTER TABLE `kategoris`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `kategori_produks`
 --
 ALTER TABLE `kategori_produks`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `keranjangs`
 --
 ALTER TABLE `keranjangs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -557,13 +677,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `pelanggans`
 --
 ALTER TABLE `pelanggans`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pembayarans`
 --
 ALTER TABLE `pembayarans`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -575,13 +695,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `pesanans`
 --
 ALTER TABLE `pesanans`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `produks`
 --
 ALTER TABLE `produks`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `rekenings`
@@ -599,7 +719,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `role_user`
 --
 ALTER TABLE `role_user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `settings`
@@ -611,13 +731,13 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT for table `sliders`
 --
 ALTER TABLE `sliders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
